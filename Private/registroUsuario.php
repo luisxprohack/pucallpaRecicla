e6cc8fa65d62e0ef1dd36866718ee6fd8fd07559
luisxprohack <luis.hack10@gmail.com>
2019-07-24 18:35:06 -0500

    first commit

---
+++ Private/registroUsuario.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Registrar usuario</title>
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600' rel='stylesheet' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+
+<div class="testbox">
+  <h1>Registration</h1>
+
+  <form action="/">
+      <hr>
+  <hr>
+  
+
+  <label id="icon" for="name"><i class="icon-user"></i></label>
+  <input type="text" name="name" id="name" placeholder="Name" required/>
+
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="Apellido" required/>
+
+  <label id="icon" for="name"><i class="icon-shield"></i></label>
+  <input type="text" name="name" id="name" placeholder="E-Mail" required/>
+
+  <label id="icon" for="name"><i class="icon-shield"></i></label>
+  <input type="text" name="name" id="name" placeholder="Celular" required/>
+
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="DNI" required/>
+
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="Password" name="name" id="name" placeholder="Password" required/>
+
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="Distrito" required/>
+
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="Dirección" required/>
+
+  <label id="icon" for="name"><i class="icon-envelope "></i></label>
+  <input type="text" name="name" id="name" placeholder="Referencia" required/>
+
+
+   <a href="#" class="button">Register</a>
+  </form>
+</div>
+</html>
---
+++ modelos/usuarios.php
@@ -0,0 +1,118 @@
+<?php
+
+  //conexion a la base de datos
+
+   require_once("../config/conexion.php");
+
+
+   class Usuarios extends Conectar {
+
+
+         public function get_filas_usuario(){
+
+            $conectar= parent::conexion();
+           
+             $sql="select * from usuarios";
+             
+             $sql=$conectar->prepare($sql);
+
+             $sql->execute();
+
+             $resultado= $sql->fetchAll(PDO::FETCH_ASSOC);
+
+             return $sql->rowCount();
+        
+        }
+
+        public function login(){
+
+            $conectar=parent::conexion();
+            parent::set_names();
+
+            if(isset($_POST["enviar"])){
+
+              //INICIO DE VALIDACIONES
+              $password = $_POST["password"];
+
+              $dni = $_POST["dni"];
+
+              
+
+                if(empty($correo) and empty($password)){
+
+                  header("Location:".Conectar::ruta()."index.php?m=2");
+                 exit();
+
+                }
+
+                 else if(!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){12,15}$/", $password)) {
+
+              header("Location:".Conectar::ruta()."index.php?m=1");
+              exit();
+
+            }
+
+             else {
+
+                  $sql= "select * from usuarios where dni=? and password=?";
+
+                  $sql=$conectar->prepare($sql);
+
+                  $sql->bindValue(1, $correo);
+                  $sql->bindValue(2, $password);
+                  $sql->execute();
+                  $resultado = $sql->fetch();
+
+                          //si existe el registro entonces se conecta en session
+                      if(is_array($resultado) and count($resultado)>0){
+
+                         /*IMPORTANTE: la session guarda los valores de los campos de la tabla de la bd*/
+                       $_SESSION["id_usuario"] = $resultado["id_usuario"];
+                       $_SESSION["nombres"] = $resultado["nombres"];
+                       $_SESSION["apellidos"] = $resultado["apellidos"];
+                       $_SESSION["dni"] = $resultado["dni"];   
+
+                        header("Location:".Conectar::ruta()."vistas/home.php");
+
+                         exit();
+
+
+                      } else {
+                          
+                          //si no existe el registro entonces le aparece un mensaje
+                          header("Location:".Conectar::ruta()."index.php?m=1");
+                          exit();
+                       }                
+                   }//cierre del else
+            }//condicion enviar
+        }
+
+        public function registrar_usuario($nombre,$apellido,$email,$celular,$dni,$contraseña,$distrito,$direccion,$ref_direccion){
+
+             $conectar=parent::conexion();
+             parent::set_names();
+
+             $sql="insert into usuarios 
+             values(null,?,?,?,?,?,?,?,?,?";
+
+             $sql=$conectar->prepare($sql);
+
+             $sql->bindValue(1, $_POST["nombres"]);
+             $sql->bindValue(2, $_POST["apellidos"]);
+             $sql->bindValue(3, $_POST["email"]);
+             $sql->bindValue(4, $_POST["celular"]);
+             $sql->bindValue(5, $_POST["dni"]);
+             $sql->bindValue(6, $_POST["contraseña"]);
+             $sql->bindValue(7, $_POST["distrito"]);
+             $sql->bindValue(8, $_POST["direccion"]);
+             $sql->bindValue(9, $_POST["ref_direccion"]);
+             $sql->execute();
+             
+              //obtenemos el valor del id del usuario
+               $id_usuario = $conectar->lastInsertId();     
+
+        }
+
+
+  }
+?>
