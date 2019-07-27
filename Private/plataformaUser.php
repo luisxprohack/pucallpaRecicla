c7c6893dbdfe872a2369f6899e8b44d379452300
cristhian <clmb.0912@gmail.com>
2019-07-27 12:13:28 -0500

    casi terminando

---
+++ Private/plataformaUser.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Ranking</title>
+
+
+<img src="Private/images/logotipo.jpeg" align="center" />
+</head>
+
+
+
+
+<body>
+	
+</body>
+
+
+
+</html>
